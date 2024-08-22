ea0a689e0b67b90f886e6c323b5a59c19d58bc77
PiyushSinghRawat <singhrawatpiyush13@gmail.com>
2024-08-22 12:50:01 +0530

    name and logo changed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  c="john"
   render() {
     return (
       <div>
-        <p>helllo this is my first react class based Component</p>
+        <p>helllo this is my first react class based Component {this.c}</p>
       </div>
     )
   }
